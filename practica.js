fef7a87866cd4228377d3ea92974266377373143
JOSÉ GILBERTO BALAM BALAM <gilbal@hotmail.com>
2021-09-30 11:40:09 -0500

    Clase Directiva V-for
    - Aprendimos a usar la directiva V-for
    - Agregamos bootstrap a nuestro proyecto mediante CDN
    - Usamos un array para llenar un SELECT
    - Creamos un objeto e hicimos PUSH a un array para agregar un nuevo objeto

--- practica.js
+++ practica.js
@@ -7,11 +7,21 @@ new Vue({
   data:{
   	mensaje:'HOLA MUNDO CRUEL again',
   	alumno:'',
+   nombre:'',
+   especie:'',
   	edad:0,
    mascotas:[{nombre:'Goldi',especie:'Canina',edad:9},
              {nombre:'Zuma',especie:'Felina',edad:4},
              {nombre:'Toño',especie:'Canina',edad:5}
             ],
+
+   especies:[
+               {clave:1,nombre:'Canina'},
+               {clave:2,nombre:'Felina'},
+               {clave:3,nombre:'Ave'}
+            ],
+
+
 
   },
 
@@ -27,6 +37,20 @@ new Vue({
       this.edad=Number(this.edad)-1;
    },
 
+   agregarMascota:function(){
+      // Construimos un objeto de tipo mascota para insertar en el array
+      var unaMascota={nombre:this.nombre,especie:this.especie,edad:this.edad};
+
+      // Agrego un objeto mascota
+      this.mascotas.push(unaMascota);
+      this.limpiarHtml();
+   },
+
+   limpiarHtml:function(){
+     this.nombre='';
+     this.especie='';
+     this.edad=''; 
+   },
    
 
 
